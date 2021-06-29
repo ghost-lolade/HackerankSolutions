9d3e4de3125f8cc2e22f8243d8170261f51f4de3
ghost-lolade <horlorlerdey10@gmail.com>
2021-06-29 18:32:14 +0100

    Counting number of valleys

---
+++ Interview Preparation Kit/Warm-up Challenges/CountingValleys.js
@@ -0,0 +1,44 @@
+// An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+// A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+// A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+// Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+// Example
+
+ 
+
+// The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+
+// Function Description
+
+// Complete the countingValleys function in the editor below.
+
+// countingValleys has the following parameter(s):
+
+// int steps: the number of steps on the hike
+// string path: a string describing the path
+// Returns
+
+// int: the number of valleys traversed
+
+function countingValleys(steps, path) {
+    // Write your code here
+    // counter for number of valleys, and level to monitor levels wrt sea
+    let counter = 0, level = 0
+    
+    // for every steps we check the current level and updatye count accourdingly
+    for (let i  = 0; i < steps; i++) {
+        if ( path[i] == 'U' ) {
+            level++;
+            // you have a valley when two conditions are met:
+            //  1. step is 'U' and 2. when level == 0 
+            if ( level == 0 ) {
+                counter++
+            }
+        }
+        else level--;
+    }
+    return counter
+
+}
