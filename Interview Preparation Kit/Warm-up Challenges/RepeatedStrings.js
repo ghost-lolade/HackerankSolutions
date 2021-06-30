fea6d19e9648a2aaead23fbe0e71db8a066baa9f
ghost-lolade <horlorlerdey10@gmail.com>
2021-07-01 00:20:52 +0100

    Repeated strings

--- Interview Preparation Kit/Warm-up Challenges/RepeatedStrings.js
+++ Interview Preparation Kit/Warm-up Challenges/RepeatedStrings.js
@@ -1,8 +1,31 @@
 // This function checks if the array
 
-// String.prototype.countCharacter = function(char) {
-//     return [...this].filter(c => c === char).length;
-//   }
+// There is a string, , of lowercase English letters that is repeated infinitely many times. Given an integer, , find and print the number of letter a's in the first  letters of the infinite string.
+
+// Example
+
+
+// The substring we consider is , the first  characters of the infinite string. There are  occurrences of a in the substring.
+
+// Function Description
+
+// Complete the repeatedString function in the editor below.
+
+// repeatedString has the following parameter(s):
+
+// s: a string to repeat
+// n: the number of characters to consider
+// Returns
+
+// int: the frequency of a in the substring
+// Input Format
+
+// The first line contains a single string, .
+// The second line contains an integer, .
+
+// Constraints
+
+// For  of the test cases, .
   
   // Complete the repeatedString function below.
   function repeatedString(s, n) {
