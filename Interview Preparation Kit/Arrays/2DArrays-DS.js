9335d35b170ef45ece2bb9ddf8aa3e45a8b9ffa9
ghost-lolade <horlorlerdey10@gmail.com>
2021-07-08 22:18:17 +0100

    better performing solution

--- Interview Preparation Kit/Arrays/2DArrays-DS.js
+++ Interview Preparation Kit/Arrays/2DArrays-DS.js
@@ -1,3 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
 function hourglassSum(arr) {
     // Write your code here
     
@@ -20,9 +51,6 @@ function hourglassSum(arr) {
                 max_sum = Math.max(max_sum, sum);
             }
         }
-
-
-        // This other solution works too
     //     let count = -Infinity
     //        for( let i = 0 ; i < 4 ; i ++)
     //     {
@@ -40,4 +68,20 @@ function hourglassSum(arr) {
     // return count
         return max_sum;
     
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = hourglassSum(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
 }
