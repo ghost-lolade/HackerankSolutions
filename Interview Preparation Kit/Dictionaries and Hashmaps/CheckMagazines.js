c210fafb30b1a6a6462f511d26a93ae2b2146c1b
ghost-lolade <horlorlerdey10@gmail.com>
2021-08-05 21:55:09 +0100

    Second solution passes all test cases

--- Interview Preparation Kit/Dictionaries and Hashmaps/CheckMagazines.js
+++ Interview Preparation Kit/Dictionaries and Hashmaps/CheckMagazines.js
@@ -13,4 +13,25 @@ function checkMagazine(magazine, note) {
 
 }
 
-// The solution above fails two test cases out of 18
+// The solution above fails two test cases out of 21
+
+function checkMagazine(magazine, note) {
+    let counter = {};
+    
+    for (let i = 0; i < magazine.length; i++) {
+        if (!counter[magazine[i]]) {
+            counter[magazine[i]] = 0;
+        }
+        counter[magazine[i]]++;
+    }
+    for (let i = 0; i < note.length; i++) {
+        if (counter[note[i]]) {
+            counter[note[i]]--;
+        } else {
+            console.log('No');
+            return;
+        }
+    }
+    console.log('Yes');
+
+}
